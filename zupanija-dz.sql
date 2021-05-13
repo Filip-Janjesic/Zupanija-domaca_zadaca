ffd30b5985f7ba735f08414227fa846d406b3aa7
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2021-05-13 19:48:14 +0200

    create table mjesto

--- zupanija-dz.sql
+++ zupanija-dz.sql
@@ -2,7 +2,6 @@ drop database if exists zupanija;
 create database zupanija;
 use zupanija;
 
-
 create table zupanija(
 sifra int not null primary key auto_increment,
 naziv varchar (50) not null,
@@ -15,17 +14,20 @@ zupanija int,
 naziv varchar (50) not null
 );
 
-alter table opcina add foreign key (zupanija) references zupanija (sifra);
+create table mjesto(
+sifra int not null primary key auto_increment,
+opcina int,
+naziv varchar (50)
+);
 
-select * from zupanija;
+create
+
+alter table opcina add foreign key (zupanija) references zupanija (sifra);
 
 insert into zupanija (naziv,zupan) values
 ('Osjecko-baranjska zupanija','Ivan Anusic'),
 ('Vukovarsko-srijemska zupanija','Bozo Galic'),
 ('Bjelovarsko-bilogorska zupanija','Damir Bajs');
-
-select * from opcina;
-
 
 insert into opcina (zupanija, naziv) values
 (1,'Bilje'),(1,'Darda'),(2,'Otok'),(2,'Ilok'),(3,'Zdenci'),(3,'Crnac');
